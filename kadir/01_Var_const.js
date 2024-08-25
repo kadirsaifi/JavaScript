1eb6e9c8848cc0e0da0eba2b02d00885f9f1c48c
Abdul Kadir <48976007+kadirsaifi@users.noreply.github.com>
2024-08-25 17:55:34 +0400

    Crate variable and const

---
+++ kadir/01_Var_const.js
@@ -0,0 +1,8 @@
+const accountId = 5282970;
+let acccountEmail = "kadir@gmail.com";
+var accoutnPassword = "64978523";
+
+//accountId = 123456;
+acccountEmail = "saifi@gmail.com";
+
+console.table([accountId, acccountEmail, accoutnPassword]);
