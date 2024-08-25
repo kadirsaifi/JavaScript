0237cc55b2043af559672b5eb9b1e0d0d38483bd
Abdul Kadir <48976007+kadirsaifi@users.noreply.github.com>
2024-08-26 00:41:03 +0400

    Add conversionOprations

---
+++ kadir/03_conversionOpration.js
@@ -0,0 +1,24 @@
+let score = true;
+
+//let numberInLength = Number(score);
+
+//console.log(typeof score);
+
+// "33"=>33
+// "33abc" => NaN
+// true=> 1; false=>0;
+
+let islogin = "";
+
+let boolenaislogin = Boolean(islogin);
+console.log(boolenaislogin);
+
+// 1 => true; 0 => false;
+// ""=> false;
+// "abdul" => true
+
+let name = 33;
+
+let StringName = String(name);
+
+console.log(typeof StringName);
