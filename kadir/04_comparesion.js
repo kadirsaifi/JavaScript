9f2775510f6a97cf8ada6caedd8d80d5b0a1151f
Abdul Kadir <48976007+kadirsaifi@users.noreply.github.com>
2024-08-28 00:03:49 +0400

    add comparesion

---
+++ kadir/04_comparesion.js
@@ -0,0 +1,14 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 <= 1);
+console.log(2 == 1);
+
+console.log("2" > 1);
+console.log("2" >= 1);
+console.log("2" == 1);
+console.log("2" < 1);
+
+//strict mode when we usee === it's mean the will check data types and value
+
+console.log(2 === 3);
